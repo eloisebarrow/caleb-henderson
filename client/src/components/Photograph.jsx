405d68c9f7039d817419f2eaeb89205853b57f61
eloisebarrow <eloisebarrow@gmail.com>
2019-11-15 16:24:59 -0500

    wip rendering local images dynamically

--- client/src/components/Photograph.jsx
+++ client/src/components/Photograph.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import './Photograph.css';
+import photos from './photos';
 
 export default function Photograph() {
 
   const [currentImgIndex, setCurrentImgIndex] = React.useState(0);
-  const placeholders = [1, 2, 3, 4, 5];
+  // const placeholders = [1, 2, 3, 4, 5];
 
-  const displayImg = placeholders.map( (img, index) => {
+  const displayImg = photos.map( (img, index) => {
     return (
-      <div 
+      <img 
         key={index}
-        onClick={() => setCurrentImgIndex(currentImgIndex !== placeholders.length - 1 ? currentImgIndex + 1 : 0)}
+        src={img.src}
+        onClick={() => setCurrentImgIndex(currentImgIndex !== photos.length - 1 ? currentImgIndex + 1 : 0)}
         className="current-img"
-        >
-          {img}
-      </div>
+        />
+
     )
   })
+
+  console.log('displayImg:', displayImg)
 
   return (
     <div className="img-container">
       {displayImg[currentImgIndex]}
+      {/* <img src={require('./../images/IMG_5149-2.jpeg')} /> */}
     </div>
   )
 }

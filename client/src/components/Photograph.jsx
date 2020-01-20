00cbdd5d400d21d6370517e6e74925faccc61911
eloisebarrow <eloisebarrow@gmail.com>
2020-01-20 09:53:50 -0500

    adding google font options

--- client/src/components/Photograph.jsx
+++ client/src/components/Photograph.jsx
@@ -5,9 +5,9 @@ import photos from './photos';
 export default function Photograph() {
 
   const [currentImgIndex, setCurrentImgIndex] = React.useState(0);
-  // const placeholders = [1, 2, 3, 4, 5];
+  const placeholders = [1, 2, 3, 4, 5];
 
-  const displayImg = photos.map( (img, index) => {
+  const displayImg = placeholders.map( (img, index) => {
     return (
       <img 
         key={index}
@@ -15,11 +15,8 @@ export default function Photograph() {
         onClick={() => setCurrentImgIndex(currentImgIndex !== photos.length - 1 ? currentImgIndex + 1 : 0)}
         className="current-img"
         />
-
     )
   })
-
-  console.log('displayImg:', displayImg)
 
   return (
     <div className="img-container">

4a5957733d87910a1cfafaadf8241dda431dcdba
Eloise Barrow <eloisebarrow@ip-192-168-1-6.ec2.internal>
2020-04-11 23:59:54 -0400

    favicon, meta content

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import './Menu.css';
 
 export default function Menu(props) {
-
-
   return (
     <div className={props.overlay ? 'show-overlay' : 'hide-overlay'}>
       <button className="closebtn" onClick={() => {props.setOverlay(false)}}>X</button>

1919182c28642272804160bb4641d9dfb252dead
Eloise Barrow <eloisebarrow@ip-192-168-1-6.ec2.internal>
2020-04-11 23:44:27 -0400

    overlay functionality complete, wip overlay animation, removed header component

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import './App.css';
 import Title from './components/Title.jsx';
 import Photograph from './components/Photograph.jsx';
-// import Header from './components/Header.jsx';
 import Contact from './components/Contact.jsx';
 import Menu from './components/Menu.jsx';
 
@@ -15,8 +14,12 @@ function App(){
       {/* <Header /> */}
       <Title />
       <Photograph />
-      <Contact overlay={overlay} setOverlay={setOverlay} />
-      <Menu overlay={overlay} setOverlay={setOverlay} />
+      <Menu 
+        overlay={overlay} 
+        setOverlay={setOverlay} /> 
+      <Contact 
+        overlay={overlay} 
+        setOverlay={setOverlay} /> 
     </div>
   );
 }
--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import './Menu.css';
 
 export default function Menu(props) {
+
+
   return (
-    <div className="overlay">
-      <button className="closebtn" onClick={() => props.setOverlay(false)}>X</button>
+    <div className={props.overlay ? 'show-overlay' : 'hide-overlay'}>
+      <button className="closebtn" onClick={() => {props.setOverlay(false)}}>X</button>
       <h1>ContactCalebH@gmail.com</h1>
     </div>
   )

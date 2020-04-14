7b21ea49a278c683831a6673498caa97418dd7d6
Eloise Barrow <eloisebarrow@ip-192-168-1-6.ec2.internal>
2020-04-13 23:11:12 -0400

    wip possible final overlay animation

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -5,7 +5,7 @@ export default function Menu(props) {
   return (
     <div className={props.overlay ? 'show-overlay' : 'hide-overlay'}>
       <button className="closebtn" onClick={() => {props.setOverlay(false)}}>X</button>
-      {/* <h1>ContactCalebH@gmail.com</h1> */}
+      <h1>ContactCalebH@gmail.com</h1>
     </div>
   )
 }

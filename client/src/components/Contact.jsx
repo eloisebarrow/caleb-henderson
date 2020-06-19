742fa45b0558eacddcb30086f88b1566a755ef83
Eloise Barrow <eloise@chartbeat.com>
2020-06-19 12:53:33 -0400

    mailto link for mobile contact

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -4,7 +4,7 @@ import './Contact.css';
 export default function Contact(props) {
   return (
     <div className="contact-container">
-      <h2 className="mobile-contact">contactCalebH@gmail.com</h2>
+      <h2 className="mobile-contact"><a href="mailto:contactcalebh@gmail.com">contactCalebH@gmail.com</a></h2>
       <h2 className="desktop-contact" onClick={() => props.toggleOverlay()}>{props.overlay ? 'X' : 'Contact'}</h2>
     </div>
   )

6a218783edd1bc216bad9f3b2fa4b19a3d48bccc
Eloise Barrow <eloise@chartbeat.com>
2020-07-17 09:23:13 -0400

    make button to close menu red

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -5,7 +5,7 @@ export default function Contact(props) {
   return (
     <div className="contact-container">
       <h2 className="mobile-contact"><a href="mailto:contactcalebh@gmail.com">contactCalebH@gmail.com</a></h2>
-      <h2 className="desktop-contact" onClick={() => props.toggleOverlay()}>{props.overlay ? 'X' : 'Contact'}</h2>
+      <h2 className="desktop-contact" onClick={() => props.toggleOverlay()}>{props.overlay ? 'x' : 'Contact'}</h2>
     </div>
   )
 }
